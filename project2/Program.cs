e26bf5f912254635785eaaf548375794e8a6a94f
Karthiga <karthigaboopathi2000@gmail.com>
2022-09-12 13:08:02 +0530

    added

---
+++ project2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"F:\project2\project_2\\studentdata.txt";
+            if (File.Exists(file))
+            {
+                string str = File.ReadAllText(file);
+                Console.WriteLine(str);
+                Console.ReadLine();
+            }
+        }
+    }
+}
